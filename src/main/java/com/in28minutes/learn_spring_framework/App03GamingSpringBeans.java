32a29e1984d31555b77007a51c12171dca7e5d83
Glowupmatt <glowupmatt@gmail.com>
2024-10-05 17:30:48 -0700

    first push

--- src/main/java/com/in28minutes/learn_spring_framework/App03GamingSpringBeans.java
+++ src/main/java/com/in28minutes/learn_spring_framework/App03GamingSpringBeans.java
@@ -1,15 +1,29 @@
 package com.in28minutes.learn_spring_framework;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import com.in28minutes.game.GameRunner;
 import com.in28minutes.game.GamingConsole;
+import com.in28minutes.game.PacManGame;
 
 
+@Configuration
 public class App03GamingSpringBeans {
-    public static void main(String[] args) {
+        @Bean
+        public GamingConsole game() {
+                PacManGame game = new PacManGame();
+                return game;
+        }
+        
+        @Bean
+        public GameRunner gameRunner(GamingConsole game) {
+                return new GameRunner(game);
+        }
+        public static void main(String[] args) {
         try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
-                GamingConfiguration.class)) {
+                App03GamingSpringBeans.class)) {
         // context.getBean(GamingConsole.class).up();
         context.getBean(GameRunner.class).run();
 }
