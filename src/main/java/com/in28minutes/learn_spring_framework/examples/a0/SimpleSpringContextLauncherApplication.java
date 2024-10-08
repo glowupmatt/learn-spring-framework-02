30de0e3eeb513b799ba974f5b127c7cdaa1a6a8c
Glowupmatt <glowupmatt@gmail.com>
2024-10-08 14:12:18 -0700

    add @service and @repository to business proj

--- src/main/java/com/in28minutes/learn_spring_framework/examples/a0/SimpleSpringContextLauncherApplication.java
+++ src/main/java/com/in28minutes/learn_spring_framework/examples/a0/SimpleSpringContextLauncherApplication.java
@@ -9,38 +9,6 @@ import org.springframework.stereotype.Component;
 import java.util.Arrays;
 
 //@Component automatically creates the beans for the class
-
-@Component
-class YourBusinessClass {
-
-
-	
-	Dependency1 dependency1;
-	Dependency2 dependency2;
-	
-	public YourBusinessClass(Dependency1 dependency1, Dependency2 dependency2) {
-		super();
-		System.out.println("Constructor Injection - YourBusinessClass");
-		this.dependency1 = dependency1;
-		this.dependency2 = dependency2;
-	}
-	
-	public String toString() {
-		return "Using " + dependency1 + " and " + dependency2;
-	}
-
-}
-
-
-@Component
-class Dependency1 {
-}
-@Component
-class Dependency2 {	
-}
-
-
-
 @Configuration
 //ComponentScan will scan everything (bean) in side the current package.
 //If you want to scan everything in the sub packages, you can use ComponentScan("com.in28minutes.learn_spring_framework")
